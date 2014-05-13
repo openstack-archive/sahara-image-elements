3645442ec9bc2e8341cba45407577b7405879d2e
Bob Nettleton <rnettleton@hortonworks.com>
2014-05-13 15:52:14 -0400

    Updates SELinux configuration in HDP images
    Recent changes to the core diskimage-builder elements
      have caused some previous warnings/errors that were silent
      to surface.  These errors are breaking the generation of the
      CentOS HDP images.
    The failure is caused by the fact that the HDP images did
      not have all the correct configuration in place to disable
      SELinux.  Disabling SELinux is required for the HDP platform.
    This patch modifies the dependencies for the hadoop-hdp
      element in order to include a dependency on the
      disable-selinux element from diskimage-builder.
    This patch removes the original SELinux configuration commands
      for HDP, since these were found to be incorrect.
    This patch adds the disable-selinux element to the HDP
      plain image as well.
    This patch also includes some element refactorings, moving
      some elements into element-deps for hadoop-hdp, since these
      are required elements for HDP.
    
    Change-Id: Ie6e26b397d022ea813c5022071bf3752ab39fa82

--- diskimage-create/diskimage-create.sh
+++ diskimage-create/diskimage-create.sh
@@ -298,7 +298,7 @@ if [ -z "$PLUGIN" -o "$PLUGIN" = "hdp" ]; then
   if [ -z "$HADOOP_VERSION" -o "$HADOOP_VERSION" = "1" ]; then
     export centos_image_name_hdp_1_3=${centos_hdp_hadoop_1_image_name:-"centos-6_4-64-hdp-1-3"}
     # Elements to include in an HDP-based image
-    centos_elements_sequence="vm rhel hadoop-hdp disable-firewall redhat-lsb sahara-version source-repositories yum"
+    centos_elements_sequence="vm rhel hadoop-hdp redhat-lsb yum"
     if [ "$DEBUG_MODE" = "true" ]; then
         # enable the root-pwd element, for simpler local debugging of images
         centos_elements_sequence=$centos_elements_sequence" root-passwd"
@@ -313,7 +313,7 @@ if [ -z "$PLUGIN" -o "$PLUGIN" = "hdp" ]; then
   if [ -z "$HADOOP_VERSION" -o "$HADOOP_VERSION" = "2" ]; then
     export centos_image_name_hdp_2_0=${centos_hdp_hadoop_2_image_name:-"centos-6_4-64-hdp-2-0"}
     # Elements to include in an HDP-based image
-    centos_elements_sequence="vm rhel hadoop-hdp disable-firewall redhat-lsb sahara-version source-repositories yum"
+    centos_elements_sequence="vm rhel hadoop-hdp redhat-lsb yum"
     if  [ "$DEBUG_MODE" = "true" ]; then
         # enable the root-pwd element, for simpler local debugging of images
         centos_elements_sequence=$centos_elements_sequence" root-passwd"
@@ -328,7 +328,7 @@ if [ -z "$PLUGIN" -o "$PLUGIN" = "hdp" ]; then
   if [ -z "$HADOOP_VERSION" -o "$HADOOP_VERSION" = "plain" ]; then
     export centos_image_name_plain=${centos_hdp_plain_image_name:-"centos-6_4-64-plain"}
     # Elements for a plain CentOS image that does not contain HDP or Apache Hadoop
-    centos_plain_elements_sequence="vm rhel redhat-lsb disable-firewall ssh sahara-version yum"
+    centos_plain_elements_sequence="vm rhel redhat-lsb disable-firewall disable-selinux ssh sahara-version yum"
     if [ "$DEBUG_MODE" = "true" ]; then
         # enable the root-pwd element, for simpler local debugging of images
         centos_plain_elements_sequence=$centos_plain_elements_sequence" root-passwd"
