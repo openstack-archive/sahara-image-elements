091330418b859dc359be9d3e44cedb7c6679ab9f
Sergey Reshetnyak <sreshetniak@mirantis.com>
2014-12-07 13:16:46 +0300

    Make bashate checks as part of pep8 checks
    Change-Id: I2b070423eff71be760b737ed1bbf7c18f32aada8

---
+++ tools/run_bashate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -xe
+
+find ./ -not -wholename \*.tox/\* -and \( -name \*.sh -or -wholename \*.d/\* -and -not -name \*.md -and -not -name \*.rst -and -not -name \*.py \) -print0 | xargs -0 bashate -v
