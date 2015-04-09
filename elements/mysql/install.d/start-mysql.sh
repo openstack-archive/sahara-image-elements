6d3bdbf50a20ca237e310d4d8f66a5e4b5dee3aa
Mikhail Ivanov <mivanov@mirantis.com>
2015-04-09 13:57:29 +0300

    Adapt not dib elements to dib-lint checks
    This scripts have been adapted to dib-lint, and now they not trigger any dib-lint error, like
    
    is not set -e
    is not set -o pipefail
    and others
    
    Partial-Bug: #1435306
    
    Change-Id: I9abf3e5a543125cfb2ae11bf51d1dc0a03010b3d

--- elements/mysql/install.d/start-mysql.sh
+++ elements/mysql/install.d/start-mysql.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "${DIB_DEBUG_TRACE:-0}" -gt 0 ]; then
+    set -x
+fi
+set -eu
+set -o pipefail
+
 if [ $(lsb_release -is) = 'Ubuntu' ]; then
     sudo service mysql start
 elif [ $(lsb_release -is) = 'Fedora' -o $(lsb_release -is) = 'CentOS' -o $(lsb_release -is) = 'RedHatEnterpriseServer' ]; then
